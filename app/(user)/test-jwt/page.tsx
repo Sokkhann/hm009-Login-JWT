0bd426f0ecf124d598e7b92e94f406a243f88790
sk <mrrkhann9u@email.com>
2024-04-20 23:55:41 +0700

    done feature refresh token

--- app/(user)/test-jwt/page.tsx
+++ app/(user)/test-jwt/page.tsx
@@ -59,17 +59,36 @@ export default function TestJWT() {
         })
 
     }
+
+    // this is method handle refresh token
+    const handleRefreshToken = async () => {
+        fetch(`http://localhost:3000/api/refresh`, {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify({})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log("Data from Refresh Token: ", data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
   return (
     <div>
         <main className='h-screen grid place-content-center'>
             <h1 className='text-5xl font-bold px-12'>
                 Testing with JWT
             </h1>
-            <button onClick={handleLogin} className='py-4 px-12 m-8 bg-red-800 rounded-lg text-gray-100 text-xl'>
+            <button onClick={handleLogin} className='py-4 px-12 my-2 bg-red-800 rounded-lg text-gray-100 text-xl'>
                 Login
             </button>
-            <button onClick={handlePartialUpdate} className='py-4 px-12 m-8 bg-red-800 rounded-lg text-gray-100 text-xl'>
+            <button onClick={handlePartialUpdate} className='py-4 px-12 my-2 bg-red-800 rounded-lg text-gray-100 text-xl'>
                 Partial Update
+            </button>
+            <button onClick={handleRefreshToken} className='py-4 px-12 my-2 bg-red-800 rounded-lg text-gray-100 text-xl'>
+                Refresh Token
             </button>
         </main>
     </div>
